8fd099936424f7dfa664a5a27746c5dc909ce0f4
tomonakar <tomohisa.nak@gmail.com>
2021-08-23 10:59:59 +0900

    chore: NX+@nx-plus/nuxtによる環境構築 @see: https://github.com/ZachJW34/nx-plus/tree/master/libs/nuxt#readme

---
+++ apps/user/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'user',
+  preset: '../../jest.preset.js',
+  transform: {
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  coverageDirectory: '../../coverage/apps/user',
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+  moduleNameMapper: {
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    'vue-jest': { tsConfig: 'apps/user/tsconfig.spec.json' },
+  },
+};
